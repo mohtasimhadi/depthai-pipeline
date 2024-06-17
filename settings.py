7b1c5dea438b3850dff2c5d901a4c31cf1171e25
mohtasimhadi <mohtasimhadi@gmail.com>
2024-06-17 14:04:05 -0500

    environmental setup

--- settings.py
+++ settings.py
@@ -6,9 +6,6 @@ with open('.env', 'r') as fh:
         for line in fh.readlines() if not line.startswith('#')
     )
 
-print(vars_dict)
 os.environ.update(vars_dict)
 
 CAM_1 = os.getenv("CAM_1_MXID")
-
-print(CAM_1)
