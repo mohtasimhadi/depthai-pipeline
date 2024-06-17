d48b7815b9f0f8087f608dfbc0afc8da73e01b5c
mohtasimhadi <mohtasimhadi@gmail.com>
2024-06-17 14:13:52 -0500

    device api created initially

---
+++ device_api.py
@@ -0,0 +1,9 @@
+import depthai as dai
+from settings import *
+
+pipeline = dai.Pipeline()
+
+with dai.Device(pipeline, dai.DeviceInfo(CAM_1)) as device:
+    print("MxId:", device.getDeviceInfo().getMxId())
+    print('USB speed:',device.getUsbSpeed())
+    print('Connected cameras:',device.getConnectedCameras())
