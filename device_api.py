7031b72bd6876fd72ecbf33823364dc241c1f7bb
mohtasimhadi <mohtasimhadi@gmail.com>
2024-06-21 16:00:11 -0500

    removed unnecessary file

--- device_api.py
+++
@@ -1,9 +0,0 @@
-import depthai as dai
-from configs.settings import *
-
-pipeline = dai.Pipeline()
-
-with dai.Device(pipeline, dai.DeviceInfo(CAM_1)) as device:
-    print("MxId:", device.getDeviceInfo().getMxId())
-    print('USB speed:',device.getUsbSpeed())
-    print('Connected cameras:',device.getConnectedCameras())
